e9776b51018c1e6fe5ab064a05100e66e3bc9cf2
OLDNINE <1120103289@qq.com>
2019-07-19 23:53:15 +0800

    实现拦截器功能

--- web/src/main/java/web/web/MyMvcConfig/MyMvcConfig.java
+++ web/src/main/java/web/web/MyMvcConfig/MyMvcConfig.java
@@ -2,20 +2,29 @@ package web.web.MyMvcConfig;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import web.web.controller.LoginHandlerInterceptor;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
     //自己写的配置类，将输入的url转到另一个页面
+    //视图映射
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/").setViewName("/index");//将/转到/index，这里的index指的是templates目录下的index，与其页面的title叫什么无关
-        registry.addViewController("/index.html").setViewName("/index");//同上
-        registry.addViewController("/main.html").setViewName("/dashboard");//同上
+
+        registry.addViewController("/").setViewName("/index");//将/转到/index?，这里的index指的是templates目录下的index，与其页面的title叫什么无关
+//        registry.addViewController("/index.html").setViewName("/index");//同上
 
     }
 
+    //注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/","/user/index");
 
+    }
 }
--- web/src/main/java/web/web/controller/LoginController.java
+++ web/src/main/java/web/web/controller/LoginController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
 public class LoginController {
 //    @RequestMapping(value = "/user/index", method = RequestMethod.GET)
     @PostMapping(value = "/user/index")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String,Object> map){
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String,Object> map, HttpSession session){
         if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+            session.setAttribute("loginUser", username);
+            //重定向
             return "redirect:/main.html";
         }else{
-            map.put("mag","用户名密码错误");
+            map.put("msg","用户名密码错误");
             return "index";
         }
 
